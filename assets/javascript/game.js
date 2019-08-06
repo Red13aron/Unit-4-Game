3678cb3943eeacbdb28a4ad067f3362a8e7f8c0f
SNelson <snelson1337@gmail.com>
2019-08-05 21:40:15 -0500

    Finished basic display, background, and started work on javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+
+//Global Variables
+//Heroes
+const hero1 = {
+    health : 150,
+    baseAttack : 15,
+    counterAttack : 10,
+    currentAttack : 15
+}
+
+const hero2 = {
+    health : 125,
+    baseAttack : 20,
+    counterAttack : 5,
+    currentAttack : 20
+}
+
+const hero3 = {
+    health : 200,
+    baseAttack : 10,
+    counterAttack : 15,
+    currentAttack : 10
+}
+
+const hero4 = {
+    health : 100,
+    baseAttack : 5,
+    counterAttack : 20,
+    currentAttack : 5
+}
+
+
+let currentHero = {}
+
+
+//Divs
+const 
+
+
+function restart() {
+    currentHero = {}
+}
